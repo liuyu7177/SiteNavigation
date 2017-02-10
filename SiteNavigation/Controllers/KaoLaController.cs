5c6001bfb4563cd3dce23cb1b54078fad9db8293
liuyu <liuyu@LAPTOP-JHAR3ID1>
2017-02-10 23:38:42 +0800

    考拉

---
+++ SiteNavigation/Controllers/KaoLaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteNavigation.Controllers
+{
+    public class KaoLaController : Controller
+    {
+        // GET: KaoLa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
