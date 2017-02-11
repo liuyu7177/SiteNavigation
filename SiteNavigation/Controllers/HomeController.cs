4b3f57358074c9d739667db995ce77d3c8fc22dd
liuyu <liuyu@LAPTOP-JHAR3ID1>
2017-02-11 23:41:54 +0800

    1

--- SiteNavigation/Controllers/HomeController.cs
+++ SiteNavigation/Controllers/HomeController.cs
@@ -18,5 +18,10 @@ namespace SiteNavigation.Controllers
         {
             return null;
         }
-	}
+        public ActionResult KaoLa()
+        {
+            string url = "http://m.kaola.com/activity/h5/20960.shtml?from=appshare&isWechatBiz=1&sharer=fAroIEnpY+PMzDpxlFJ3iIr359kyMQAbHh7dT9AMSzXEcXMQMmXVFtXpfg0NFhP+cTD36FxU4TaMnnF7Vj7jXQ==#wechatBiz";
+            return Redirect(url);
+        }
+    }
 }
