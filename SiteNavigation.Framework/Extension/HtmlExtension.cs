2764962a30a689bb07433cb541e0443e097b9b3f
liuyu <liuyu@LAPTOP-JHAR3ID1>
2017-02-25 23:33:23 +0800

    1

---
+++ SiteNavigation.Framework/Extension/HtmlExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Configuration;
+namespace SiteNavigation.Framework.Extension
+{
+    public static class HtmlExtension
+    {
+        private static string baseDic = ConfigurationManager.AppSettings["baseDic"]?? "/Content/";
+        public static string ImageInclude(this HtmlHelper html,string url)
+        {
+            return baseDic + url;
+        }
+    }
+}
--- SiteNavigation/Controllers/HomeController.cs
+++ SiteNavigation/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace SiteNavigation.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
+           
             return View();
         }
         public ActionResult Test()
