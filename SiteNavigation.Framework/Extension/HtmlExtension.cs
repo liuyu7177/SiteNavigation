464f309ff9bf24591675b41edd3729f737b1c8ed
liuyu <liuyu@LAPTOP-JHAR3ID1>
2017-02-26 17:51:29 +0800

    1

--- SiteNavigation.Framework/Extension/HtmlExtension.cs
+++ SiteNavigation.Framework/Extension/HtmlExtension.cs
@@ -10,9 +10,46 @@ namespace SiteNavigation.Framework.Extension
     public static class HtmlExtension
     {
         private static string baseDic = ConfigurationManager.AppSettings["baseDic"]?? "/Content/";
+        private static string isCompressionStr = ConfigurationManager.AppSettings["baseDic"] ?? "true";
+        private static bool isCompression = false;
+        private static string _version = "20160101";
+        static HtmlExtension()
+        {
+            if(isCompressionStr == "true")
+            {
+                isCompression = true;
+            }
+        }
         public static string ImageInclude(this HtmlHelper html,string url)
         {
-            return baseDic + url;
+            return baseDic + url+"?"+ _version;
+        }
+        public static string StaticSource(this HtmlHelper html, string url)
+        {
+            return baseDic + url + "?" + _version; ;
+        }
+
+        public static string CssSource(this HtmlHelper html, string url)
+        {
+            if (isCompression)
+            {
+                return baseDic + "css/compression/" + url + "?" + _version; ;
+            }
+            else
+            {
+                return baseDic + "css/source/" + url + "?" + _version; ;
+            }            
+        }
+        public static string JsSource(this HtmlHelper html, string url)
+        {
+            if (isCompression)
+            {
+                return baseDic + "Js/compression/" + url + "?" + _version; ;
+            }
+            else
+            {
+                return baseDic + "Js/source/" + url + "?" + _version; ;
+            }
         }
     }
 }
---
+++ SiteNavigation/Code/SiteNavigationObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SiteNavigation.Code
+{
+    public class SiteNavigationObject
+    {
+        private static string baseDic = ConfigurationManager.AppSettings["DomainName"];
+
+        public static int StatisticalCode()
+        {
+            switch (baseDic)
+            {
+                case "jmkaola":
+                    return 1;
+                case "zuori":
+                    return 2;
+                default: break;
+            }
+            return 0;
+        }
+    }
+}
